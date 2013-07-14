ed45f764a8f46409d955431e795112b3d5892c2f
Michael Mathieu <mathieu.cs.nyu.edu>
2013-07-13 18:58:08 -0600

    Fake neurons create place cells, nice hexagonsls

--- python/SLAM.py
+++ python/SLAM.py
@@ -17,15 +17,21 @@ class SLAM():
         self.nPerVCO = nPerVCO
         
         # Place cells:
-#         placeCellsCo = [i for i in itertools.product(range(nPerVCO), range(nPerVCO))]
-#         self.nPlaceCells = len(placeCellsCo)
-#         self.wPlaceCells = numpy.zeros([self.nPlaceCells, self.nVCO*self.nPerVCO])
-#         for k in xrange(self.nPlaceCells):
-#             for i in xrange(self.nVCO):
-#                 for j in xrange(self.nPerVCO):
-#                     self.wPlaceCells[k, i*self.nPerVCO + j] = 1/70
-#         self.placeCells = [lif.LIF(R=100, abs_ref = 0.005) for i in xrange(self.nPlaceCells)]
-#         self.placeCellsOutputs = numpy.zeros([self.nPlaceCells])        
+        placeCellsCo = [[0,0,0],[0,1,0],[0,2,0],[0,3,0],
+                        [0,0,1],[0,1,1],[0,2,1],[0,3,1],
+                        [0,0,2],[0,1,2],[0,2,2],[0,3,2],
+                        [0,0,3],[0,1,3],[0,2,3],[0,3,3]]
+        #placeCellsCo = [i for i in itertools.product(range(nPerVCO), range(nPerVCO))]
+        self.nPlaceCells = len(placeCellsCo)
+        self.wPlaceCells = numpy.zeros([self.nPlaceCells, self.nVCO*self.nPerVCO])
+        for k in xrange(self.nPlaceCells):
+            for i in xrange(self.nVCO):
+                self.wPlaceCells[k, i*self.nPerVCO + placeCellsCo[k][i]] = 1.
+        self.placeCells = [lif.LIF(R=20, abs_ref = 0.005) for i in xrange(self.nPlaceCells)]
+        self.placeCellsOutputs = numpy.zeros([self.nPlaceCells])        
+
+        self.testPC = [[] for i in xrange(self.nPlaceCells)]
+        self.t = 0
         
     def update(self, dx, dy, dt, robot = None, gui = None):
         # VCO:
@@ -35,28 +41,44 @@ class SLAM():
             for j in xrange(self.nPerVCO):
                 I = 1 * max(self.VCOoutputs[i][j],0)
                 self.VCOlifoutputs[i][j] = self.VCOlif[i][j].update(I, dt)
+
         # debug
-        if (robot != None) and (gui != None):
-            #if controlOutput > 0.5:
-            #    gui.point(robot.x, robot.y, color=(0,0,0), width = 5)
-            for i in xrange(len(self.VCOlifoutputs)):
-                outputLine = self.VCOlifoutputs[i]
-                for j in xrange(len(outputLine)):
-                    output = outputLine[j]
-                    if (output > 40) and (controlOutput > 0.5):
-                        color = [0,0,0,0.5]
-                        color[i] = float(j+1)/4
-                        gui.point(robot.x, robot.y, color=tuple(color), width = 4)
+#         if (robot != None) and (gui != None):
+#             colors = [i for i in itertools.product([0.,0.5,1.], [0.,0.5,1.], [0.,0.5,1.])]
+#             #if controlOutput > 0.5:
+#             #    gui.point(robot.x, robot.y, color=(0,0,0), width = 5)
+#             for i in xrange(len(self.VCOlifoutputs)):
+#                 outputLine = self.VCOlifoutputs[i]
+#                 #for j in xrange(len(outputLine)):
+#                 for j in xrange(1):
+#                     output = outputLine[j]
+#                     if (output > 40):# and (controlOutput > 0.5):
+#                         #color = [0,0,0,0.5]
+#                         #color[i] = float(j+1)/4
+#                         color = colors[i*4+j]
+#                         gui.point(robot.x, robot.y, color=tuple(color), width = 4)
 
         # Place cells:
-#         I = self.wPlaceCells * self.VCOlifoutputs.reshape(self.nVCO*self.nPerVCO)
+        I = self.wPlaceCells.dot(self.VCOlifoutputs.reshape(self.nVCO*self.nPerVCO,1))
 #         for i in xrange(self.nPlaceCells):
 #             cell = self.placeCells[i]
-#             V = cell.update(self.VCO, I[i], dt)
+#             V = cell.update(I[i], dt)
 #             self.placeCellsOutputs[i] = V
+        self.t += dt
+        for i in xrange(self.nPlaceCells):
+            if I[i].squeeze() > 40:
+                self.testPC[i].append((self.t,I[i].squeeze()/50))
+        deltat = 10./(8*2*math.pi*10)
+        self.testPC = [[(t,a) for (t,a) in x if t > self.t-deltat] for x in self.testPC]
+        #print self.testPC
+        self.placeCellsOutputs = [sum([a for (t,a) in x]) for x in self.testPC]
 
         # debug
-#         colors = [i for i in itertools.product([0., 0.5, 1.], 
-#         if (robot != None) and (gui != None):
-#             for i in xrange(self.nPlaceCells):
+        colors = [i for i in itertools.product([0.,0.5,1.], [0.,0.5,1.], [0.,0.5,1.])]
+        if (robot != None) and (gui != None):
+            for i in xrange(self.nPlaceCells):
+                #if self.placeCellsOutputs[i] > 40:
+                if self.placeCellsOutputs[i] > 2.5:
+                    print i
+                    gui.point(robot.x, robot.y, color=tuple(colors[i]), width=4)
                 
--- python/main.py
+++ python/main.py
@@ -2,17 +2,22 @@ import display
 import gtk
 import robot
 import SLAM
+import math
 
 if __name__=="__main__":
     gui = display.GUI(scale = 400., wpixels = 400., hpixels=400.)
     robot = robot.Robot(x = 0.1, y = 0.1, theta = 3.14/2, noise = 0.3, gui=gui,
                         velocity = 0.2)
-    slam = SLAM.SLAM(VCOdirs=[[0,1]])
-    nSubIters = 100
+    dirs=[[1,0],[-0.5,math.sqrt(3.)/2],[-0.5,-math.sqrt(3.)/2]]
+    dirs = [[0.5*x,0.5*y] for x,y in dirs]
+    slam = SLAM.SLAM(VCOdirs=dirs)
+    nSubIters = 50
     try:
         while True:
             while gtk.events_pending():
                 gtk.main_iteration()
+                
+            # Main loop : put code here
             Dt = 0.05
             Dx, Dy = robot.update(Dt)
             dx = Dx/nSubIters
@@ -20,8 +25,6 @@ if __name__=="__main__":
             dt = Dt/nSubIters
             for i in range(nSubIters):
                 slam.update(dx, dy, dt, robot, gui)
-                
-                
-            
+        
     except KeyboardInterrupt:
         pass
