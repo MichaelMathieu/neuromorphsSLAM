e9a9e04e45c4d61bf6e4a3ca70ed61ad1593886c
Michael Mathieu <mathieu@cs.nyu.edu>
2013-07-15 19:26:41 -0600

    Revert "Revert "Add polar plots""
    This reverts commit 1ffcb7d6f15d07aab74a660d23fdd41d25d59e0c.

--- python/SLAM.py
+++ python/SLAM.py
@@ -3,6 +3,7 @@ import math
 import VCO
 import lif
 import itertools
+import plots
 
 class SLAM():
     def __init__(self, VCOdirs=[[0,1],[1,0],[0,-1],[-1,0]], nPerVCO=4):
@@ -18,22 +19,31 @@ class SLAM():
         self.nPerVCO = nPerVCO
         
         # Place cells:
-        placeCellsCo = [[0,0,0],[0,1,0],[0,2,0],[0,3,0],
-                        [0,0,1],[0,1,1],[0,2,1],[0,3,1],
-                        [0,0,2],[0,1,2],[0,2,2],[0,3,2],
-                        [0,0,3],[0,1,3],[0,2,3],[0,3,3]]
+        placeCellsCo0= [[0,0,0],[0,1,0],[0,2,0],[0,3,0],
+                         [0,0,1],[0,1,1],[0,2,1],[0,3,1],
+                         [0,0,2],[0,1,2],[0,2,2],[0,3,2],
+                         [0,0,3],[0,1,3],[0,2,3],[0,3,3]]
+        placeCellsCo  = [[x  ,y  ,z  ,None,None,None] for x,y,z in placeCellsCo0]
+        # placeCellsCo += [[None,None,None,x,y,z] for x,y,z in placeCellsCo0]
+        #placeCeallsCo = [[0,0,0,0,0,0],[0,2,2,0,1,1],[0,0,2,0,0,1],[0,2,0,0,1,0]]
         #placeCellsCo = [i for i in itertools.product(range(nPerVCO), range(nPerVCO))]
         self.nPlaceCells = len(placeCellsCo)
+        print(self.nPlaceCells)
         self.wPlaceCells = numpy.zeros([self.nPlaceCells, self.nVCO*self.nPerVCO])
         for k in xrange(self.nPlaceCells):
             for i in xrange(self.nVCO):
-                self.wPlaceCells[k, i*self.nPerVCO + placeCellsCo[k][i]] = 1./15
+                co = placeCellsCo[k][i]
+                if co != None:
+                    self.wPlaceCells[k, i*self.nPerVCO + co] = 1./15
         C = 1
-        R = 2.3*1./(C*1e-9*math.log(2.)*2*math.pi*Omega)/1e6
+        #R = 2.3*1./(C*1e-9*math.log(2.)*2*math.pi*Omega)/1e6
+        R = 2.*1./(C*1e-9*math.log(2.)*2*math.pi*Omega)/1e6
+        #R = 1.3*1./(C*1e-9*math.log(2.)*2*math.pi*Omega)/1e6
         self.placeCells = lif.LIFBank(self.nPlaceCells, R=R, C=C, abs_ref=0.005)
 
         self.testPC = [[] for i in xrange(self.nPlaceCells)]
         self.t = 0
+        self.plotphases = None
         
     def update(self, dx, dy, dt, robot = None, gui = None):
         # VCO:
@@ -45,20 +55,22 @@ class SLAM():
                 self.VCOlifoutputs[i][j] = self.VCOlif[i][j].update(I, dt)
 
         # debug
-#         if (robot != None) and (gui != None):
-#             colors = [i for i in itertools.product([0.,0.5,1.], [0.,0.5,1.], [0.,0.5,1.])]
-#             #if controlOutput > 0.5:
-#             #    gui.point(robot.x, robot.y, color=(0,0,0), width = 5)
-#             for i in xrange(len(self.VCOlifoutputs)):
-#                 outputLine = self.VCOlifoutputs[i]
-#                 #for j in xrange(len(outputLine)):
-#                 for j in xrange(1):
-#                     output = outputLine[j]
-#                     if (output > 40):# and (controlOutput > 0.5):
-#                         #color = [0,0,0,0.5]
-#                         #color[i] = float(j+1)/4
-#                         color = colors[i*4+j]
-#                         gui.point(robot.x, robot.y, color=tuple(color), width = 4)
+        # if (robot != None) and (gui != None):
+        #     colors = [i for i in itertools.product([0.,0.5,1.], [0.,0.5,1.], [0.,0.5,1.])]
+        #     #if controlOutput > 0.5:
+        #     #    gui.point(robot.x, robot.y, color=(0,0,0), width = 5)
+        #     for i in xrange(len(self.VCOlifoutputs)):
+        #         outputLine = self.VCOlifoutputs[i]
+        #         for j in xrange(len(outputLine)):
+        #         #for j in xrange(1):
+        #             output = outputLine[j]
+        #             if (output > 40) and (controlOutput > 0.5):
+        #                 #color = [0,0,0,0.5]
+        #                 #color[i] = float(j+1)/4
+        #                 color = colors[i*4+j]
+        #                 gui.point(robot.x, robot.y, color=tuple(color), width = 4)
+        #phases = [vco.phase - self.controlVCO.phase for vco in self.VCOs]
+        #self.plotphases = plots.phaseplot(phases, figure = self.plotphases)
 
         # Place cells:
         I = self.wPlaceCells.dot(self.VCOlifoutputs.reshape(self.nVCO*self.nPerVCO,1)).squeeze()
@@ -73,11 +85,13 @@ class SLAM():
 #         self.placeCellsOutputs = [sum([a for (t,a) in x]) for x in self.testPC]
 
         # debug
-        colors = [i for i in itertools.product([0.,0.5,1.], [0.,0.5,1.], [0.,0.5,1.])]
+        colorsBase = [0.,0.5,1.]
+        colors = [i for i in itertools.product(colorsBase, colorsBase, colorsBase)]
         if (robot != None) and (gui != None):
             for i in xrange(self.nPlaceCells):
                 if self.placeCellsOutputs[i] > 40:
                 #if self.placeCellsOutputs[i] > 2.5:
-                    #print i
+                    print str(i) + " spiking"
                     gui.point(robot.x, robot.y, color=tuple(colors[i]), width=4)
+                        
                 
