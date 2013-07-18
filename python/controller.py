0f2eb7b3b4586862cbf0ab1d7d308203da0897f1
Michael Mathieu <mathieu@cs.nyu.edu>
2013-07-18 01:46:45 -0600

    Restored bump sensors

--- python/controller.py
+++ python/controller.py
@@ -11,37 +11,39 @@ class controllerAbstraction(object):
       exit(-1)
 
 class guiController(controllerAbstraction):
-   def __init__(self, gui, obstacles):
+   def __init__(self, gui, obstacles, speed = 0.033):
       self.gui = gui
       self.obstacles = obstacles
       self.dtheta_obs_avoidance = 0.1*math.pi
       self.theta = 0
-      self.speed = 0.033
+      self.speed = speed
 
    def updateControl(self, robot, dt):
-        incr_theta = 5. # in rad PER SECOND
-        dtheta = 0.
-        dx = 0
-        dy = 0
-        if self.gui.keyState("Left"):
-            dtheta -= incr_theta * dt
-        if self.gui.keyState("Right"):
-            dtheta += incr_theta * dt
-        origTheta = self.theta
-        self.theta += dtheta * dt
-        self.theta += random.gauss(0, robot.noise)
-        nCollisions = 0
-        theta0 = self.theta
-        for x1, y1, x2, y2 in self.obstacles:
-            nCollisions += self.avoidLine(x1, y1, x2, y2, dt, robot, True)
-        if nCollisions > 1:
-            self.theta = theta0 + math.pi
-        dx = self.speed*dt*cos(self.theta)
-        dy = self.speed*dt*sin(self.theta)
-        if self.gui:
-            self.gui.line(robot.x, robot.y, robot.x+dx, robot.y+dy,
-                              color=(0,0,1), width=1)
-        return dx, dy
+      incr_theta = 5. # in rad PER SECOND
+      dtheta = 0.
+      dx = 0
+      dy = 0
+      if self.gui.keyState("Left"):
+         dtheta -= incr_theta * dt
+      if self.gui.keyState("Right"):
+         dtheta += incr_theta * dt
+      origTheta = self.theta
+      self.theta += dtheta * dt
+      self.theta += random.gauss(0, robot.noise)
+      nCollisions = 0
+      theta0 = self.theta
+      for x1, y1, x2, y2 in self.obstacles:
+         nCollisions += self.avoidLine(x1, y1, x2, y2, dt, robot, True)
+      if nCollisions > 1:
+         self.theta = theta0 + math.pi
+      if any(robot.getBumpSensors()):
+         self.theta = theta0 + math.pi
+      dx = self.speed*dt*cos(self.theta)
+      dy = self.speed*dt*sin(self.theta)
+      if self.gui:
+         self.gui.line(robot.x, robot.y, robot.x+dx, robot.y+dy,
+                       color=(0,0,1), width=1)
+      return dx, dy
 
    def avoidLine(self, x1, y1, x2, y2, dt, robot, smooth = True):
         a = y2-y1
--- python/robot.py
+++ python/robot.py
@@ -18,30 +18,34 @@ class RealRobot(RobotAbstraction):
       wheelradius = 0.0225
       beta = 360./(math.pi*2.*wheelradius) * self.cheatFactor
       self.robotCoordinateMatrix = numpy.matrix(
-          [[-beta*math.sqrt(3)/2, -beta*0.5, alpha],
-           [                   0,      beta, alpha],
-           [ beta*math.sqrt(3)/2, -beta*0.5, alpha]])
+         [[-beta*math.sqrt(3)/2, -beta*0.5, alpha],
+          [                   0,      beta, alpha],
+          [ beta*math.sqrt(3)/2, -beta*0.5, alpha]])
       self.robotTargetWheelsPosition = numpy.matrix(self.rif.get("Wi"),dtype='float').T
-   
+      
    def update(self, dx, dy):
-       dtheta = 0
-       dpos = numpy.matrix([[dx],[dy],[dtheta]])
-       dwheels = self.robotCoordinateMatrix * dpos
-       self.robotTargetWheelsPosition += dwheels
-       self.rif.setW(self.robotTargetWheelsPosition[0,0],
-                     self.robotTargetWheelsPosition[1,0],
-                     self.robotTargetWheelsPosition[2,0])
-       self.x += dx
-       self.y += dy
-       return self.x, self.y
+      dtheta = 0
+      dpos = numpy.matrix([[dx],[dy],[dtheta]])
+      dwheels = self.robotCoordinateMatrix * dpos
+      self.robotTargetWheelsPosition += dwheels
+      self.rif.setW(self.robotTargetWheelsPosition[0,0],
+                    self.robotTargetWheelsPosition[1,0],
+                    self.robotTargetWheelsPosition[2,0])
+      self.x += dx
+      self.y += dy
+      return self.x, self.y
+
+   def getBumpSensors(self):
+      return self.rif.bumpData
 
 class SimRobot(RobotAbstraction):
-    def __init__(self, x = 0., y = 0., noise = 0.01):
-       super(SimRobot, self).__init__(x, y, noise)
+   def __init__(self, x = 0., y = 0., noise = 0.01):
+      super(SimRobot, self).__init__(x, y, noise)
+      
+   def update(self, dx, dy):
+      self.x += dx
+      self.y += dy
+      return self.x, self.y
 
-    def update(self, dx, dy):
-       self.x += dx
-       self.y += dy
-       return self.x, self.y
-        
-
+   def getBumpSensors(self):
+      return [False for i in xrange(6)]
